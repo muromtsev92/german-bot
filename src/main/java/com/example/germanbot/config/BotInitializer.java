41c9479de032f384b77ecf17e745705ccff98367
SergeiMuromtsev <k29393@yandex.ru>
2023-12-31 00:30:22 +0500

    Revert "command menu added"
    This reverts commit c528067be0ba7dc4b4befd73520574dddfc4bd62.

--- src/main/java/com/example/germanbot/config/BotInitializer.java
+++ src/main/java/com/example/germanbot/config/BotInitializer.java
@@ -24,7 +24,7 @@ public class BotInitializer {
         try {
             telegramBotsApi.registerBot(telegramBot);
         } catch (TelegramApiException e) {
-            log.error("Error: " + e.getMessage() + "!");
+            log.error("Error: " + e.getMessage());
         }
     }
 }
